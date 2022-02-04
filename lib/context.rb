a4605c8d7ad7ea3d0877aa508217408cce059a35
Yodanishe <fire.demon@mail.ru>
2022-02-04 12:56:23 +0700

    rework menu and welcome

--- lib/context.rb
+++ lib/context.rb
@@ -1,21 +1,25 @@
-require_relative 'states/exit'  
+require_relative 'states/exit'
 require_relative 'states/welcome'
 require_relative 'states/base_state'
+require_relative 'valera'
+require_relative 'action'
 
 class Context
+  attr_accessor :valera, :actions
 
-    def initialize  (state)
-        transition_to_state state
-    end
+  def initialize(state)
+    @valera = Valera.new
+    @actions = ActionLoader.new('actions.yml').load
+    transition_to_state state
+  end
 
-    def repeat_state
-        @state.run
-    end
+  def repeat_state
+    @state.run
+  end
 
-    def transition_to_state (state)
-        @state = state
-        @state.context = self
-        @state.run
-    end
-    
+  def transition_to_state(state)
+    @state = state
+    @state.context = self
+    @state.run
+  end
 end
